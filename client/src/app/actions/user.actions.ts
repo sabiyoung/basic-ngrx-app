301039a3d8c92e9118bf4683af47d544b5087e3a
saba <sabera@alphaworks.tech>
2022-06-29 22:21:42 -0700

    feat(action): create user action

--- client/src/app/actions/user.actions.ts
+++ client/src/app/actions/user.actions.ts
@@ -1,4 +1,5 @@
 import { createAction, props } from '@ngrx/store';
+import { User } from '../interfaces/user.model';
 
 export const loadUsers = createAction(
   '[User] Load Users'
@@ -12,4 +13,8 @@ export const loadUsersSuccess = createAction(
 export const loadUsersFailure = createAction(
   '[User] Load Users Failure',
   props<{ error: any }>()
+);
+export const actionAddUser = createAction(
+  '[Users] add User',
+  props<{ user: User }>()
 );
