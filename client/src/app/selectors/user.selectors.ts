9fe41444e07bebbb94c9762010baff1ebb99cba3
saba <sabera@alphaworks.tech>
2022-06-29 22:22:02 -0700

    feat(selector): create user selector

--- client/src/app/selectors/user.selectors.ts
+++ client/src/app/selectors/user.selectors.ts
@@ -1,2 +1,10 @@
 import { createFeatureSelector, createSelector } from '@ngrx/store';
 
+import * as fromUsers from '../reducers/user.reducer';
+
+const getUsersFeature = createFeatureSelector<fromUsers.State>(fromUsers.userFeatureKey);
+
+export const selectUsers = createSelector(
+  getUsersFeature,
+  (state) => state.users
+);
