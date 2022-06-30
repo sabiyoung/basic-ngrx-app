85ba58cbb59c329e7afcaabecfa50f9f6cb60b96
saba <sabera@alphaworks.tech>
2022-06-29 22:41:06 -0700

    feat(reducer): add selectedUser state

--- client/src/app/reducers/user.reducer.ts
+++ client/src/app/reducers/user.reducer.ts
@@ -1,15 +1,18 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { actionAddUser } from '../actions/user.actions';
 import { User } from '../interfaces/user.model';
+import { actionSelectUserId } from './../actions/user.actions';
 
 export const userFeatureKey = 'user';
 
 export interface State {
   users: User[];
+  selectedId: number | null;
 }
 
 export const initialState: State = {
   users: [],
+  selectedId: null
 };
 
 export const reducer = createReducer(
@@ -20,6 +23,14 @@ export const reducer = createReducer(
       return {
         ...state,
         users: [...state.users, action.user],
+      }
+    }),
+    on(actionSelectUserId,(
+      state, action
+    ) => {
+      return {
+        ...state,
+        selectedId: action.id
       }
     })
 );
