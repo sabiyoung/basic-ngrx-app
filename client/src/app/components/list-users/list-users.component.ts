c56d7eb8627c4140bc820607597cd5317efe8c98
saba <sabera@alphaworks.tech>
2022-06-29 22:39:34 -0700

    feat(users): display users

--- client/src/app/components/list-users/list-users.component.ts
+++ client/src/app/components/list-users/list-users.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { actionSelectUserId } from 'src/app/actions/user.actions';
+import { State } from 'src/app/reducers';
+import { selectUsers } from 'src/app/selectors/user.selectors';
 
 @Component({
   selector: 'app-list-users',
@@ -6,10 +10,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-users.component.scss']
 })
 export class ListUsersComponent implements OnInit {
-
-  constructor() { }
+  users$ = this.store.select(selectUsers);
+  constructor(private store: Store<State>) {}
 
   ngOnInit(): void {
   }
+ 
 
 }
